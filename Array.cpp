44506313d8d00930bd557852cb98f48470b4b50c
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-09 18:53:52 +0700

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	//ARRAY
+	
+//	int bilangan[5]={1,2,3};	
+//	cout<<bilangan[2];
+
+	//ARRAY KARAKTER
+//	char huruf[5]={'a','b','c','d','e'}
+//	cout<<huruf[1];
+	
+	//STRING ADALAH ARRAY OF CHART
+//	string nama="ADAM";
+//	cout<<nama[1];
+	
+//	string nama={"ADAM","ISAL","YANTO"};
+//	cout<<nama[4];
+	
+	//CARA MENGAMBIL DATA DARI ARRAY
+//	string nama={"ADAM","ISAL","YANTO"};
+//	cout<<nama[0]<<end;
+//	cout<<nama[1]<<endl;
+
+	//CONTOH LAIN
+//	string nama={"ADAM","ISAL","YANTO","D","A"};
+//	cout<<nama[0]<<end;
+//	nama[0]="HANIF";
+//	cout<<nama[1]<<endl;
+	
+	//
+//	int nilai[5]={20,32,33,35,36};
+//	for(int i=0 ; i<5 ; i++){
+//		cout<<"Nilai ke-"<<i<<" = "<<nilai[i]<<endl;
+//	}
+	
+	//CARA INPUT ARRAY
+//	int nilai[5];
+//	for(int i=0 ; i<5 ; i++)
+//
+//	cout<<"Nilai ke-"<<i<<" = "<<nilai[i]<<endl;
+//	} (belum selesai)
+	
+	//MELIHAT PANJANG ARRAY
+//	string nama="assdahdkdsjgfskabkjnsbveurnlvnseakhggajh";
+//	string nama[]={"ADAM","ISAL","YANTO","D","A"};
+//	cout<<sizeof(nama)/sizeof(nama[0]);
+	
+	//CONTOH LAIN
+//	string nama[]={"ADAM","ISAL","YANTO","D","A"};
+//	for(int=0 ; i<sizeof(nama)/sizeof(nama[0]) ; i++);
+// belum selesai
+
+	//ARRAY 2 DIMENSI
+	int matriks [50][50];
+	int jum_b.jum_k,i,j;
+	
+	cout<<"Jumlah baris: ";
+	cin>>jum_b;
+	
+	cout<<"Jumlah kolom: ";//belum selesai
+	
+	
+}
